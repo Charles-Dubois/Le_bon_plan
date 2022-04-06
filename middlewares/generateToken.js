98d655ad6df9cef062b954941079bd49ad57e98b
Charles Dubois <charles.dubois.h@gmail.com>
2022-04-06 11:42:10 +0200

    working on bcrypt compare

--- middlewares/generateToken.js
+++ middlewares/generateToken.js
@@ -4,19 +4,16 @@ const Login = require("../models/loginModel");
 const bcrypt = require("bcrypt");
 
 async function signupToken(req, res, next) {
-  let result, name, ID;
+  let result;
   try {
     req.body.password = await bcrypt.hash(req.body.password, 12);
     result = await Login.create(req.body);
-    ID = result.id;
-    name = req.body.name;
   } catch (error) {
-    console.log(`Error from /middlewares/generateToken.js`);
+    console.log(`Error from /middlewares/generateToken.js in signupToken`);
     console.error(error);
-    console.log(`Error from /middlewares/generateToken.js`);
+    console.log(`Error from /middlewares/generateToken.js in signupToken`);
     return res.status(400).json({ message: "This name is not availaible" });
   }
-
   const token = jwt.sign(
     {
       data: "leBonPlan",
@@ -24,11 +21,43 @@ async function signupToken(req, res, next) {
     },
     SECRET
   );
-
-  res.cookie("leBonPlan", token, { httpOnly: true, secure: false });
-
+  res.cookie("lebonplan", token, { httpOnly: true, secure: false });
   next();
 }
 
-const generateToken = { signupToken };
+async function loginToken(req, res, next) {
+  const errorMessage = "Name or password not valid";
+  let result;
+  try {
+    req.body.password = await bcrypt.hash(req.body.password, 12);
+    result = await Login.find({ name: req.body.name });
+
+    if (result.length === 0) {
+      return res.json({ message: `The name ${req.body.name} is not valid` });
+    }
+    const ckeckPassword = await bcrypt.compare(
+      { password: req.body.password },
+      result.password
+    );
+    if (!ckeckPassword) {
+      return res.json(errorMessage);
+    }
+  } catch (error) {
+    console.log(`Error from /middlewares/generateToken.js in loginToken`);
+    console.error(error);
+    console.log(`Error from /middlewares/generateToken.js in loginToken`);
+    return res.status(400).json({ message: "Name is not valid" });
+  }
+  const token = jwt.sign(
+    {
+      data: "leBonPlan",
+      id: result.id,
+    },
+    SECRET
+  );
+  res.cookie("lebonplan", token, { httpOnly: true, secure: false });
+  next();
+}
+
+const generateToken = { signupToken, loginToken };
 module.exports = generateToken;
--- router/loginRouter.js
+++ router/loginRouter.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const Login = require("../models/loginModel");
 const bcrypt = require("bcrypt");
 const { login } = require("../middlewares/coditionJoi");
+const { loginToken } = require("../middlewares/generateToken");
 router.get("/", (req, res) => {
   res.render("login");
 });
 
-router.post("/", login, async (req, res, next) => {
-  console.log(req.cookies);
-  next();
+router.post("/", login, loginToken, async (req, res, next) => {
+  const name = req.body.name;
+
+  res.render("profile", { name });
 });
 module.exports = router;
